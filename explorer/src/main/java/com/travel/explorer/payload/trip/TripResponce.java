76b9081d99fe4559751a0ff25ea89a4296cd59c6
Julya <julya.meleshko@gmail.com>
2026-03-31 12:25:05 +0300

    update TripResponce, remove PeopleAlsoSearch

--- explorer/src/main/java/com/travel/explorer/payload/trip/TripResponce.java
+++ explorer/src/main/java/com/travel/explorer/payload/trip/TripResponce.java
@@ -1,14 +1,16 @@
 package com.travel.explorer.payload.trip;
 
+import com.travel.explorer.entities.Trip;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class TripResponce {
-  private String title;
-  private String desc;
-  private String startDate;
-  private String endDate;
-  private List<Day> days;
+  private Trip trip;
+//  private String title;
+//  private String desc;
+//  private String startDate;
+//  private String endDate;
+//  private List<Day> days;
 }
 
