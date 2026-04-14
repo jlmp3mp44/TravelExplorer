530b77ab1d402bc1ac609939c914e9d0a9f68647
Julya Meleshko <julya.meleshko@gmail.com>
2026-04-14 15:07:38 +0200

    Revert "classes declaration"
    This reverts commit affa6e538b61ccf7295b6b5e45077bcf473ef2d4.

--- explorer/src/main/java/com/travel/explorer/entities/VectorScoredPlace.java
+++
@@ -1,6 +0,0 @@
-package com.travel.explorer.entities;
-
-// not db entity (table) but will be pulled from db
-public class VectorScoredPlace extends Place{
-    public float vectorScore;
-}
--- explorer/src/main/java/com/travel/explorer/payload/trip/TripCreationRequest.java
+++
@@ -1,5 +0,0 @@
-package com.travel.explorer.payload.trip;
-
-// CHANGE IT TO SPRING APPROACH
-public class TripCreationRequest {
-}
