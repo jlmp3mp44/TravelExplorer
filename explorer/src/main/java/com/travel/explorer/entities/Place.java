09c18f972b9a80ea938685b338f470e8cfc724a8
yuliia.hl <julya.meleshko@gmail.com>
2026-04-14 15:07:38 +0200

    Revert "classes declaration"
    This reverts commit affa6e538b61ccf7295b6b5e45077bcf473ef2d4.

--- explorer/src/main/java/com/travel/explorer/entities/Place.java
+++ explorer/src/main/java/com/travel/explorer/entities/Place.java
@@ -1,22 +1,26 @@
 package com.travel.explorer.entities;
 
-import jakarta.persistence.CascadeType;
+import com.travel.explorer.entities.embeddable.Location;
+import com.travel.explorer.entities.embeddable.OpenHours;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
 @Table(name = "places")
@@ -30,18 +34,57 @@ public class Place {
   @Column(name = "place_id")
   private Long id;
 
+  /** Google Places API short id (for dedup / reference). */
+  @Column(name = "google_place_id", length = 256)
+  private String googlePlaceId;
+
   @NotBlank(message = "can not be blank")
   @Size(min = 3, max = 50, message = "Title should contains between 3 and 50 characters")
   @Column(name = "title")
   private String title;
   @Column(name = "description")
   private String desc;
-  @Column(name = "photo")
-  private String photo;
-  @Column(name = "rating")
-  private float rating;
+  @ManyToMany
+  @JoinTable(
+      name = "place_category",
+      joinColumns = @JoinColumn(name = "place_id"),
+      inverseJoinColumns = @JoinColumn(name = "category_id")
+  )
+  private List<Category> categories;
+  @Column(name = "address")
+  private String address;
+  @Column(name = "neighborhood")
+  private String neighborhood;
+  @Column(name = "city")
+  private String city;
+  @Column(name = "street")
+  private String street;
+  @Column(name = "state")
+  private String state;
+  @Column(name = "phone")
+  private String phone;
+  @Embedded
+  private Location location;
+  @Column (name = "totalScore")
+  private Integer totalScore;
+  @Column (name = "permanentlyClosed")
+  private Boolean permanentlyClosed;
+  @Column(name = "temporarilyClosed")
+  private Boolean temporarilyClosed;
+  @ElementCollection
+  @CollectionTable(name = "place_open_hours", joinColumns = @JoinColumn(name = "place_id"))
+  private List<OpenHours> openHours;
 
 
+  @ManyToMany
+  @JoinTable(
+      name = "place_placesPeopleSearch",
+      joinColumns = @JoinColumn(name = "place_id"),
+      inverseJoinColumns = @JoinColumn(name = "related_place_id")
+  )
+  private List<Place> placesPeopleSearches;
+  @Column(name = "photoUrl")
+  private String photoUrl;
   @ManyToMany(mappedBy = "places")
-  private List<Trip> trips =  new ArrayList<>();
+  private List<Activity> activities =  new ArrayList<>();
 }
--- explorer/src/main/java/com/travel/explorer/service/recommendation/ITripPlanner.java
+++
@@ -1,8 +0,0 @@
-package com.travel.explorer.service.recommendation;
-
-import com.travel.explorer.entities.Trip;
-import com.travel.explorer.payload.trip.TripCreationRequest;
-
-public interface ITripPlanner {
-    public Trip plan(TripCreationRequest request);
-}
