14f74946598282592db89887a4d19b4a9cb8ee85
Julya <julya.meleshko@gmail.com>
2026-04-22 13:29:32 +0300

    update tables a little

--- explorer/src/main/java/com/travel/explorer/entities/Activity.java
+++ explorer/src/main/java/com/travel/explorer/entities/Activity.java
@@ -1,5 +1,6 @@
 package com.travel.explorer.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -48,6 +50,12 @@ public class Activity {
   @Column(name = "user_added", nullable = false)
   @ColumnDefault("false")
   private Boolean userAdded = false;
+
+  @OneToMany(mappedBy = "activity",
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  private List<ActivityRating
+      > ratings = new ArrayList<>();
 
   private LocalDateTime startTime;
   private LocalDateTime endTime;
